8442ff3fa8dbba35976ea74ec1712b4f21d3a2a3
pejoal <pejoal.official@gmail.com>
2024-08-15 22:10:18 +0200

    Remove Unnecessary Files 1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,15 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Lobby;
 use Inertia\Inertia;
 
 class HomeController extends Controller {
 
   public function index() {
-    $lobbies = Lobby::get()->toArray();
     return Inertia::render('Home', [
-      'lobbies' => $lobbies,
     ]);
   }
 
