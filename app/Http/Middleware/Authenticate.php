6ae1554aa889bf61673b2af1d969c47752df44de
pejoal <pejoal.official@gmail.com>
2024-08-20 21:01:53 +0200

    secure_url login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,19 +3,21 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Support\Facades\App;
 
-class Authenticate extends Middleware
-{
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
-    {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+class Authenticate extends Middleware {
+  /**
+   * Get the path the user should be redirected to when they are not authenticated.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return string|null
+   */
+  protected function redirectTo($request) {
+    if (!$request->expectsJson()) {
+      if (App::environment('production')) {
+        return redirect(secure_url(route('login')));
+      }
+      return route('login');
     }
+  }
 }
