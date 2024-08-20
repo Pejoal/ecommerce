9d68ccd31fa6f20d30b4dbb516ff402669be1506
pejoal <pejoal.official@gmail.com>
2024-08-20 20:02:14 +0200

    remove axios base

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,7 +6,6 @@
 
 import axios from "axios";
 window.axios = axios;
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
 
 window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
