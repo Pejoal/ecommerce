9e913fe59e0651eb7bfb3ea9baf1ad5fd069e2c3
pejoal <pejoal.official@gmail.com>
2024-08-22 00:21:13 +0200

    CustomVerifyEmail

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\CustomVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -47,6 +48,10 @@ class User extends Authenticatable implements MustVerifyEmail {
   protected $casts = [
     'email_verified_at' => 'datetime',
   ];
+
+  public function sendEmailVerificationNotification() {
+    $this->notify(new CustomVerifyEmail);
+  }
 
   function products() {
     return $this->hasMany(Product::class, 'created_by');
