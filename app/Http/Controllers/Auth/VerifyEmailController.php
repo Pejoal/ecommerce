fb429479e67e33b63a55b3180d5c660b836b0afa
pejoal <pejoal.official@gmail.com>
2024-08-21 22:46:56 +0200

    verify email 1

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -8,21 +8,19 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\RedirectResponse;
 
-class VerifyEmailController extends Controller
-{
-    /**
-     * Mark the authenticated user's email address as verified.
-     */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
-    {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
-        }
-
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
-
-        return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
+class VerifyEmailController extends Controller {
+  /**
+   * Mark the authenticated user's email address as verified.
+   */
+  public function __invoke(EmailVerificationRequest $request): RedirectResponse {
+    if ($request->user()->hasVerifiedEmail()) {
+      return redirect()->intended(route('home') . '?verified=1');
     }
+
+    if ($request->user()->markEmailAsVerified()) {
+      event(new Verified($request->user()));
+    }
+
+    return redirect()->intended(route('home') . '?verified=1');
+  }
 }
