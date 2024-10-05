c086737a2b33339e6958afbbf0c6454deed329e0
pejoal <pejoal.official@gmail.com>
2024-10-05 18:02:27 +0200

    Update project details and styling in Portfolio.vue

--- resources/js/Pages/Portfolio.vue
+++ resources/js/Pages/Portfolio.vue
@@ -18,22 +18,25 @@ AOS.init();
 
 const projects = ref([
   {
-    title: "Project 1",
-    description: "Description for Project 1",
+    title: "E-commerce",
+    description: "E-commerce project",
     link: "https://github.com/Pejoal/ecommerce",
-    logo: "https://github.com/Pejoal/ecommerce/logo.png",
+    liveLink: "https://user.github.io/project1",
+    image: "logo.jpg",
   },
   {
     title: "Project 2",
-    description: "Description for Project 2",
-    link: "https://github.com/Pejoal/project2",
-    logo: "https://github.com/Pejoal/project2/logo.png",
+    description: "This is a description of Project 1.",
+    link: "https://github.com/user/project1",
+    liveLink: "https://user.github.io/project1",
+    image: "logo.jpg",
   },
   {
     title: "Project 3",
-    description: "Description for Project 3",
-    link: "https://github.com/Pejoal/project3",
-    logo: "https://github.com/Pejoal/project3/logo.png",
+    description: "This is a description of Project 1.",
+    link: "https://github.com/user/project1",
+    liveLink: "https://user.github.io/project1",
+    image: "logo.jpg",
   },
 ]);
 
@@ -66,37 +69,51 @@ const skills = ref({
     class="min-h-screen bg-gray-100 flex flex-col justify-center items-center"
   >
     <div class="bg-white shadow rounded-3xl w-full sm:max-w-2xl p-2">
-      <section>
-        <h2 class="font-bold">Pejoal Hanna (21)</h2>
-        <div
-          class="indent-2 block font-semibold text-xl self-start text-gray-700"
-        >
-          <p class="text-sm text-gray-500 font-normal">Full Stack Developer</p>
-          <p class="text-sm text-gray-500 font-normal">Rees, Germany</p>
+      <section class="p-6 bg-white shadow-lg rounded-lg">
+        <h2 class="font-bold text-2xl mb-4">Pejoal Hanna (21)</h2>
+        <div class="text-gray-700">
+          <p class="text-sm text-gray-500 font-normal mb-2">
+            Full Stack Developer
+          </p>
+          <p class="text-sm text-gray-500 font-normal mb-2">Rees, Germany</p>
           <p class="text-sm text-gray-500 font-normal">Full Stack Developer</p>
         </div>
       </section>
 
-      <section class="border-t my-2">
-        <section class="text-base text-gray-700 sm:text-lg" data-aos="fade-up">
-          <h2 class="font-semibold pb-2">Projects</h2>
-          <section
-            v-for="project in projects"
-            :key="project.title"
-            class="border-b"
-          >
-            <p class="text-blue-500">{{ project.title }}</p>
-            <p>{{ project.description }}</p>
-            <a :href="project.link">
+      <section class="border-t my-2 text-gray-700" data-aos="fade-up">
+        <h2 class="font-semibold pb-2">Projects</h2>
+        <section
+          v-for="project in projects"
+          :key="project.title"
+          class="border rounded-lg shadow-lg p-6 mb-6 bg-white"
+        >
+          <img
+            :src="project.image"
+            :alt="project.title"
+            class="w-full h-64 object-cover rounded-md"
+          />
+          <p class="text-blue-500 text-xl font-bold mt-4">
+            {{ project.title }}
+          </p>
+          <p class="mt-2">{{ project.description }}</p>
+          <div class="mt-4">
+            <a
+              :href="project.link"
+              class="inline-block text-blue-500 hover:text-blue-700 mr-4"
+            >
               <FontAwesomeIcon :icon="faGithub" class="mr-1" />
               GitHub
             </a>
-          </section>
+            <a
+              :href="project.liveLink"
+              class="inline-block text-blue-500 hover:text-blue-700"
+            >
+              Live
+            </a>
+          </div>
         </section>
 
-        <section
-          class="pt-6 text-base leading-6 font-bold sm:text-lg sm:leading-7"
-        >
+        <section class="pt-4 text-base leading-6 font-bold sm:text-lg">
           <p>
             Contact me at
             <a href="mailto:pejoal.official@gmail.com" class="text-blue-500"
@@ -106,10 +123,10 @@ const skills = ref({
         </section>
       </section>
 
-      <section class="border-t my-2">
+      <section class="border-t">
         <!-- Tech Skills Section -->
         <section
-          class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+          class="py-4 text-base text-gray-700 sm:text-lg"
           data-aos="zoom-out"
         >
           <h2 class="font-bold text-2xl">Skills</h2>
@@ -118,11 +135,7 @@ const skills = ref({
               {{ groupName }}
             </h3>
             <ul class="list-disc list-inside">
-              <li
-                v-for="skill in skillGroup"
-                :key="skill"
-                class="text-zinc-800"
-              >
+              <li v-for="skill in skillGroup" :key="skill" class="indent-2">
                 <FontAwesomeIcon :icon="skill.icon" class="mr-1" />
                 {{ skill.name }}
               </li>
@@ -132,20 +145,15 @@ const skills = ref({
 
         <!-- Skills Section -->
         <section
-          class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+          class="py-4 text-base text-gray-700 sm:text-lg"
           data-aos="zoom-in"
         >
-          <h2 class="font-bold text-2xl">Skills</h2>
           <div v-for="(skillGroup, groupName) in skills" :key="groupName">
             <h3 class="text-blue-500 font-semibold text-lg">
               {{ groupName }}
             </h3>
             <ul class="list-disc list-inside">
-              <li
-                v-for="skill in skillGroup"
-                :key="skill"
-                class="text-zinc-800"
-              >
+              <li v-for="skill in skillGroup" :key="skill" class="indent-2">
                 {{ skill }}
               </li>
             </ul>
@@ -154,7 +162,7 @@ const skills = ref({
 
         <!-- Languages Section -->
         <section
-          class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+          class="py-4 text-base text-gray-700 sm:text-lg"
           data-aos="flip-left"
         >
           <h2 class="font-bold text-2xl">Languages</h2>
@@ -166,9 +174,7 @@ const skills = ref({
         </section>
 
         <!-- Jobs Section -->
-        <section
-          class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
-        >
+        <section class="py-4 text-base text-gray-700 sm:text-lg">
           <h2 class="font-bold text-2xl">Jobs</h2>
           <ul>
             <li v-for="job in jobs" :key="job">{{ job }}</li>
@@ -176,9 +182,7 @@ const skills = ref({
         </section>
 
         <!-- Certificates Section -->
-        <section
-          class="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
-        >
+        <section class="py-4 text-base text-gray-700 sm:text-lg">
           <h2 class="font-bold text-2xl">Certificates</h2>
           <ul>
             <li v-for="certificate in certificates" :key="certificate">
