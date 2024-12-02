d92d37702c73ea1065eb88a0cf4ad72ca89459dd
pejoal <pejoal.official@gmail.com>
2024-12-02 19:11:00 +0100

    Update input binding in Zana Index.vue to use 'Rechnung Nr' for Verwendungszweck

--- resources/js/Pages/Zana/Index.vue
+++ resources/js/Pages/Zana/Index.vue
@@ -305,7 +305,7 @@ watch(
         >
         <input
           id="verwendungszweck"
-          v-model="form.title"
+          v-model="form['Rechnung Nr']"
           type="text"
           placeholder="Enter Verwendungszweck"
           class="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
