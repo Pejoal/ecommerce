f6fc3ecf490e707ee67e27da93dda5803a926f76
pejoal <pejoal.official@gmail.com>
2025-01-12 00:37:44 +0100

    Remove notification permission request and native notification functionality from Portfolio.vue

--- resources/js/Pages/Portfolio.vue
+++ resources/js/Pages/Portfolio.vue
@@ -162,30 +162,6 @@ const certificates = ref([
     date: "April 2022",
   },
 ]);
-
-// Request permission for notifications
-if (Notification.permission !== "granted") {
-  Notification.requestPermission().then((permission) => {
-    if (permission === "granted") {
-      console.log("Notification permission granted.");
-    }
-  });
-}
-
-// Send a native notification
-function sendNotification(title, options) {
-  if (Notification.permission === "granted") {
-    new Notification(title, options);
-  } else {
-    console.log("Notification permission not granted.");
-  }
-}
-
-// Example: Send a notification
-sendNotification("New Message", {
-  body: "You have a new message!",
-  icon: "/path/to/icon.png", // Optional: Add an icon
-});
 </script>
 
 <template>
